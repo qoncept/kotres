b049dca9f04bd98c855c6727e020121ea782ba28
Yuta Koshizawa <koher@koherent.org>
2016-11-25 16:14:45 +0900

    Fix `mp` and `ap`, and add tests for them

--- src/jp/co/qoncept/util/Operators.kt
+++ src/jp/co/qoncept/util/Operators.kt
@@ -1,10 +1,10 @@
 package jp.co.qoncept.util
 
-fun <T, R, E: Exception> ((T) -> R).mp(result: Result<T, E>): Result<R, E> {
+infix fun <T, R, E: Exception> ((T) -> R).mp(result: Result<T, E>): Result<R, E> {
     return result.map(this)
 }
 
-fun <T, R, E: Exception> Result<(T) -> R, E>.ap(result: Result<T, E>): Result<R, E> {
+infix fun <T, R, E: Exception> Result<(T) -> R, E>.ap(result: Result<T, E>): Result<R, E> {
     return result.apply(this)
 }
 
--- test/jp/co/qoncept/util/OperatorsTest.kt
+++ test/jp/co/qoncept/util/OperatorsTest.kt
@@ -7,6 +7,49 @@ import jp.co.qoncept.util.Result.*
 
 class OperatorsTest {
     @Test
+    fun mpAndAp() {
+        run {
+            val foo = curry(::Foo) mp Success<Int, AnimalException>(42) ap Success<Boolean, DogException>(true) ap Success<String, CatException>("ABC")
+            assertEquals(foo.value?.i, 42)
+            assertEquals(foo.value?.b, true)
+            assertEquals(foo.value?.s, "ABC")
+        }
+
+        run {
+            val e = AnimalException()
+            val foo = curry(::Foo) mp Failure<Int, AnimalException>(e) ap Success<Boolean, DogException>(true) ap Success<String, CatException>("ABC")
+            assertEquals(foo.exception, e)
+        }
+
+        run {
+            val e = DogException()
+            val foo = curry(::Foo) mp Success<Int, AnimalException>(42) ap Failure<Boolean, DogException>(e) ap Success<String, CatException>("ABC")
+            assertEquals(foo.exception, e)
+        }
+
+        run {
+            val e = CatException()
+            val foo = curry(::Foo) mp Success<Int, AnimalException>(42) ap Success<Boolean, DogException>(true) ap Failure<String, CatException>(e)
+            assertEquals(foo.exception, e)
+        }
+
+        run {
+            val e1 = AnimalException()
+            val e2 = DogException()
+            val e3 = CatException()
+            val foo = curry(::Foo) mp Failure<Int, AnimalException>(e1) ap Failure<Boolean, DogException>(e2) ap Failure<String, CatException>(e3)
+            assertEquals(foo.exception, e3)
+        }
+
+        run {
+            val e1 = AnimalException()
+            val e2 = DogException()
+            val foo = curry(::Foo) mp Failure<Int, AnimalException>(e1) ap Failure<Boolean, DogException>(e2) ap Success<String, CatException>("ABC")
+            assertEquals(foo.exception, e2)
+        }
+    }
+
+    @Test
     fun or() {
         run {
             val a = Success<Int, AnimalException>(42)
@@ -69,4 +112,10 @@ class OperatorsTest {
             assertTrue(b.exception is CatException)
         }
     }
+}
+
+private data class Foo(val i: Int, val b: Boolean, val s: String)
+
+private fun <A, B, C, Z> curry(f: (A, B, C) -> Z): (A) -> (B) -> (C) -> Z {
+    return { a -> { b -> { c -> f(a, b, c) } } }
 }
