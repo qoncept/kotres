de2c5f4e7857993ade114dbbb44ccb34e79eed8f
Yuta Koshizawa <koher@koherent.org>
2016-11-25 15:40:16 +0900

    Implement `or`

--- src/jp/co/qoncept/util/Operators.kt
+++ src/jp/co/qoncept/util/Operators.kt
@@ -7,3 +7,17 @@ fun <T, R, E: Exception> ((T) -> R).mp(result: Result<T, E>): Result<R, E> {
 fun <T, R, E: Exception> Result<(T) -> R, E>.ap(result: Result<T, E>): Result<R, E> {
     return result.apply(this)
 }
+
+infix fun <T, E: Exception> Result<T, E>.or(alternative: T): T {
+    return when (this) {
+        is Result.Success -> value
+        is Result.Failure -> alternative
+    }
+}
+
+infix fun <T, E: Exception> Result<T, E>.or(alternative: Result<T, E>): Result<T, E> {
+    return when (this) {
+        is Result.Success -> this
+        is Result.Failure -> alternative
+    }
+}
